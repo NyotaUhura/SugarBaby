359a44c7e8c78ea009ff5641aaafc657b669d4b4
Ira Kosenkova <Lrinakos2002@gmail.com>
2021-12-05 11:40:21 +0200

    add settings UI

--- app/src/main/java/com/example/sbaby/SettingsFragment.kt
+++ app/src/main/java/com/example/sbaby/SettingsFragment.kt
@@ -1,30 +1,78 @@
 package com.example.sbaby
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.sbaby.auth.FirebaseAuthManager
 import com.example.sbaby.databinding.FragmentSettingsBinding
-import com.example.sbaby.databinding.FragmentSignUpBinding
+import com.example.sbaby.epoxy.simpleController
+import com.example.sbaby.epoxy.viewholders.settings.SettingsCardViewHolder
+import com.example.sbaby.epoxy.viewholders.settings.settingsCardViewHolder
 import org.koin.android.ext.android.getKoin
 
-class SettingsFragment : Fragment() {
+data class Setting(
+    val id: Int,
+    val image: Int,
+    val name: String,
+)
+
+class SettingsFragment : MvRxBaseFragment(R.layout.fragment_settings) {
     private val binding: FragmentSettingsBinding by viewBinding()
     private val authManager: FirebaseAuthManager by getKoin().inject()
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+
+    private val buttons: SettingsCardViewHolder.buttonsOnclick =
+        object : SettingsCardViewHolder.buttonsOnclick {
+            override fun editProfileOnClick() {
+                TODO("Not yet implemented")
+            }
+
+            override fun viewFamilyOnClick() {
+                TODO("Not yet implemented")
+            }
+
+            override fun changeLanguageOnClick() {
+                TODO("Not yet implemented")
+            }
+
+            override fun changePasswordOnClick() {
+                TODO("Not yet implemented")
+            }
+
+            override fun privatePolicyOnClick() {
+                TODO("Not yet implemented")
+            }
+
+            override fun rateOnClick() {
+                TODO("Not yet implemented")
+            }
+
+            override fun logOutOnClick() {
+                authManager.logOut()
+            }
+
+        }
+
+    private val settingsList = mutableListOf<Setting>()
+
+    override fun epoxyController() = simpleController() {
+        settingsList.add(Setting(1, R.drawable.ic_edit, "Edit profile photo"))
+        settingsList.add(Setting(2, R.drawable.ic_family, "Family"))
+        settingsList.add(Setting(3, R.drawable.ic_language, "Language"))
+        settingsList.add(Setting(4, R.drawable.ic_password, "Change password"))
+        settingsList.add(Setting(5, R.drawable.ic_private, "Privacy Policy"))
+        settingsList.add(Setting(6, R.drawable.ic_rate, "Rate the app"))
+        settingsList.add(Setting(7, R.drawable.ic_log_out, "Log out"))
+        settingsList.forEach { s ->
+            settingsCardViewHolder {
+                id(s.id)
+                setting(s)
+                onClickListeners(buttons)
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.logOutButton.setOnClickListener {
-            authManager.logOut()
-        }
+        binding.recyclerView.requestModelBuild()
     }
 }
---
+++ app/src/main/java/com/example/sbaby/epoxy/viewholders/settings/SettingsCardViewHolder.kt
@@ -0,0 +1,43 @@
+package com.example.sbaby.epoxy.viewholders.settings
+
+import com.airbnb.epoxy.EpoxyAttribute
+import com.airbnb.epoxy.EpoxyModelClass
+import com.example.sbaby.R
+import com.example.sbaby.Setting
+import com.example.sbaby.ViewBindingEpoxyModelWithHolder
+import com.example.sbaby.databinding.ItemSettingsBinding
+
+@EpoxyModelClass(layout = R.layout.item_settings)
+abstract class SettingsCardViewHolder : ViewBindingEpoxyModelWithHolder<ItemSettingsBinding>() {
+    @EpoxyAttribute
+    lateinit var setting: Setting
+
+    @EpoxyAttribute(EpoxyAttribute.Option.DoNotHash)
+    open lateinit var onClickListeners: buttonsOnclick
+
+    override fun ItemSettingsBinding.bind() {
+        imageView.setImageResource(setting.image)
+        textView.text = setting.name
+        settingsCard.setOnClickListener {
+            when (setting.id) {
+                1 -> onClickListeners.editProfileOnClick()
+                2 -> onClickListeners.viewFamilyOnClick()
+                3 -> onClickListeners.changeLanguageOnClick()
+                4 -> onClickListeners.changePasswordOnClick()
+                5 -> onClickListeners.privatePolicyOnClick()
+                6 -> onClickListeners.rateOnClick()
+                7 -> onClickListeners.logOutOnClick()
+            }
+        }
+    }
+
+    interface buttonsOnclick {
+        fun editProfileOnClick()
+        fun viewFamilyOnClick()
+        fun changeLanguageOnClick()
+        fun changePasswordOnClick()
+        fun privatePolicyOnClick()
+        fun rateOnClick()
+        fun logOutOnClick()
+    }
+}
